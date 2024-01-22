bde10b4c9c7a985cf46a04998e2cf38add3e88c6
ymd65536 <ymd65536@gmail.com>
2024-01-22 23:30:00 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # distroless_langchain_image
+
 distrolessイメージでLangChainのイメージを作成する
+
+```bash
+cd distroless
+docker build -t test_distroless:1 .
+docker run -it --rm test_distroless:1
+```
---
+++ alpine/main.py
@@ -0,0 +1,11 @@
+# import time
+# from typing import List
+
+import langchain
+# from langchain.chat_models import ChatVertexAI
+# from langchain.embeddings import VertexAIEmbeddings
+# from langchain.llms import VertexAI
+# from langchain.llms import OpenAI
+# from langchain.schema import HumanMessage, SystemMessage
+
+print(f"LangChain version: {langchain.__version__}")
